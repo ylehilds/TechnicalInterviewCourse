cf897b8bf1b7fcf440b162a5cd20a6dfcf82f84d
ylehilds <lehi_alcantara@byu.edu>
2022-01-06 10:17:36 -0700

    modified comments

--- homework/Recursion_Assignment/recursion_assigment.js
+++ homework/Recursion_Assignment/recursion_assigment.js
@@ -16,7 +16,7 @@
 
 function powerOf2(n) {
   if (n === 1) return true // if after all the divisions you get n to be 1 then this n is a power of 2
-  if (n % 2 === 0) return powerOf2(n/2) // if the mod 2 (divisions by 2 has no remaining then recursively call itself with n divided by 2)
+  if (n % 2 === 0) return powerOf2(n/2) // if the mod/remainder 2 (divisions by 2 that has no remainder then recursively call itself with n divided by 2)
   return false // if you reach this point in the code return false as this n is not a power of 2
   // alternatively w/out recursion I could simply make that determination by the below code:
   // return (n%2 === 0)
