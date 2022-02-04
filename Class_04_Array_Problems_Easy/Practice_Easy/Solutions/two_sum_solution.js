1bccbc9ce5c3d113cf01f5c3cee98e50a5de97a7
ylehilds <lehi_alcantara@byu.edu>
2022-02-04 00:40:21 -0700

    added one more task.

---
+++ Class_04_Array_Problems_Easy/Practice_Easy/Solutions/two_sum_solution.js
@@ -0,0 +1,17 @@
+// # Given an array of integers, return indices of the two numbers such that they add up to a specific target
+// # You may assume that each input would have exactly one solution, and you may not use the same element twice
+// # This problem came from leetcode.com
+
+input_array = [2, 5, 6, 3, 9] 
+input_target = 9
+// # Output = [2, 3]
+
+function twoSum(array, target) {
+    for (let i=0;i<input_array.length;i++) {
+        for (let j=1;j<input_array.length;j++) {
+            if (input_array[i] + input_array[j] === target) return [i, j]
+        }
+    }
+}
+
+console.log(twoSum(input_array, input_target))
