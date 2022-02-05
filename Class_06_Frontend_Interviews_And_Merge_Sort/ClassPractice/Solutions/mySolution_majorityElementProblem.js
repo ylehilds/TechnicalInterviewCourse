173d1ba0e2defe8dc360d19f74aea58ef7bc29b5
ylehilds <lehi_alcantara@byu.edu>
2022-02-05 11:30:46 -0700

    finished the implementation for mySolution_majorityElementProblem method.

--- Class_06_Frontend_Interviews_And_Merge_Sort/ClassPractice/Solutions/mySolution_majorityElementProblem.js
+++ Class_06_Frontend_Interviews_And_Merge_Sort/ClassPractice/Solutions/mySolution_majorityElementProblem.js
@@ -15,16 +15,29 @@ Source: https://leetcode.com/problems/majority-element/
 const arr1 = [3, 2, 3] // 3
 const arr2 = [2, 2, 1, 1, 1, 2, 2] // 2
 
-let dictionary = {}
-function findMajority(array) {
-  for (let num of array) {
-    if (!dictionary.hasOwnProperty(num)) {
-      dictionary[num]
-    } else {
-
-    }
-  }
+function mergeSort(array) {
+  const half = array.length/2
+  if (array.length < 2) return array // recursion base case
+  const left = array.slice(0, half)
+  const right = array.slice(half, array.length)
+  return merge(mergeSort(left), mergeSort(right)) //divide and conquer, keep splitting (recursion) until left/right is an array of 1, merge as to sort left/right recursively
 }
 
-// not finished
+function merge (left, right) {
+  const sortedArray = []
+  while(left.length && right.length) {
+    if (left[0] < right[0]) sortedArray.push(left.shift()) // compare first number from left & right and push the lower number to sortedArray while removing it from left/right accordingly
+    else sortedArray.push(right.shift())
+  }
+  return [...sortedArray, ...left, ...right] //to account for any left overs from left/right, just append/add it to sortedArray, the appending data would be already sorted
+}
+
+function findMajority(array) {
+  let sortedArray = mergeSort(array)
+  let middleIndex = Math.floor(sortedArray.length / 2)
+  return sortedArray[middleIndex]
+}
+
+console.log(`the majority element for arr1 is: ${findMajority(arr1)}`)
+console.log(`the majority element for arr2 is: ${findMajority(arr2)}`)
 
