0a9284040e8e575a1f1d339ababb3bcd7edbc476
ylehilds <lehi_alcantara@byu.edu>
2022-02-05 20:52:27 -0700

    got it working.

--- Class_08_Dictionary_Problems_Medium/myRemoveDuplicate.js
+++ Class_08_Dictionary_Problems_Medium/myRemoveDuplicate.js
@@ -1,12 +1,36 @@
-test_dict = {'Manjeet' : [1, 4, 5, 6],
-  'Akash' : [1, 8, 9],
+test_dict = {
+  'Manjeet': [1, 4, 5, 6],
+  'Akash': [1, 8, 9],
   'Nikhil': [10, 22, 4],
-  'Akshat': [5, 11, 22]}
+  'Akshat': [5, 11, 22]
+}
 
 function removeDuplicates(dict) {
-  let duplicateDictionary = {}
-  let values =  Object.values(dict)
-
+  let duplicateArray = []
+  let values = Object.values(dict).flat()
+  let ogValues = Object.values(dict)
+  let numsFrequency = getDictionary(values)
+  let numsArray = Object.entries(numsFrequency)
+  for (const [number, frequency] of numsArray) {
+    if (frequency > 1) duplicateArray.push(number)
   }
+  for (let duplicate of duplicateArray) {
+    for (let value of ogValues) {
+      for (let [index, numValue] of value.entries()) {
+        if (numValue === parseInt(duplicate)) value.splice(index, 1)
+      }
+    }
+  }
+  return dict
+}
 
-  // not finished in class
+function getDictionary(nums) {
+  let dictHelper = {}
+  for (let num of nums) {
+    dictHelper[num] ? dictHelper[num] += 1 : dictHelper[num] = 1
+  }
+  return dictHelper
+}
+
+console.log(removeDuplicates(test_dict))
+// not finished in class
