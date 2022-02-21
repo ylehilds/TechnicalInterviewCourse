158c15d140b63bd8ad15381685e6735899c81632
ylehilds <lehi_alcantara@byu.edu>
2022-02-20 22:17:43 -0700

    fixed a bug

--- Class_04_Array_Problems_Easy/Practice_Easy/Solutions/two_sum_solution.js
+++ Class_04_Array_Problems_Easy/Practice_Easy/Solutions/two_sum_solution.js
@@ -3,15 +3,17 @@
 // # This problem came from leetcode.com
 
 input_array = [2, 5, 6, 3, 9] 
-input_target = 9
+input_target = 10
 // # Output = [2, 3]
 
 function twoSum(array, target) {
     for (let i=0;i<input_array.length;i++) {
         for (let j=1;j<input_array.length;j++) {
+            if (i === j) continue // don't add the same number together
             if (input_array[i] + input_array[j] === target) return [i, j]
         }
     }
+    return false
 }
 
 console.log(twoSum(input_array, input_target))
