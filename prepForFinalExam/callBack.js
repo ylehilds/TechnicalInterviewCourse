f0da6b7b8ed6599a43b25d2082413cd7435bb59b
ylehilds <lehi_alcantara@byu.edu>
2022-02-22 20:48:54 -0700

    added some notes and explanations

--- prepForFinalExam/callBack.js
+++ prepForFinalExam/callBack.js
@@ -1,5 +1,6 @@
 // 8. What is a callback function?
 //   - A callback function is a function passed into another function as an argument, which is then invoked inside the outer function to complete some kind of routine or action.
+// Simply put, callbacks are just functions that are passed in as arguments to other functions. Period.
 
 function action(x, y, callback) {
   return callback(x, y);
